38f6fd4c00d4fa76f762addc0ad3ae9de6bf9793
jeongho kim <kjh123qwa@naver.com>
2020-04-16 19:50:01 +0900

    Updates

---
+++ precache-manifest.92084fdc9b07de9d1c525e69884354e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac804ebfe49f520c9e1e4f4483bebe26",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "9b067a642cfb73873fb8",
+    "url": "/movie_app_2019/static/css/main.2d145220.chunk.css"
+  },
+  {
+    "revision": "dc163bd3a331881cd7e4",
+    "url": "/movie_app_2019/static/js/2.6db49de2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.6db49de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b067a642cfb73873fb8",
+    "url": "/movie_app_2019/static/js/main.59ca557e.chunk.js"
+  },
+  {
+    "revision": "5e5823dcf47d14d23ffd",
+    "url": "/movie_app_2019/static/js/runtime-main.4d23df71.js"
+  }
+]);
